9e871ddd7bf79689781cdb608c1ac7ece05ef4ee
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-06-02 13:43:49 +0300

    Package script support for minor versions

--- package.sh
+++ package.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 APP_NAME="steam-giveaways-bot-launcher"
-VERSION=$(date +"%y.%j")
+VERSION_MAJOR=$(date +"%y.%j")
+VERSION_MINOR="$1"
 RELEASE_DIR_RELATIVE="bin/Release"
 PUBLISH_DIR_RELATIVE="${RELEASE_DIR_RELATIVE}/publish-script-output"
 RELEASE_DIR="$(pwd)/$RELEASE_DIR_RELATIVE"
 PUBLISH_DIR="$(pwd)/$PUBLISH_DIR_RELATIVE"
+
+if [ -z $VERSION_MINOR ]; then
+    VERSION_MINOR="0"
+fi
+
+VERSION="$VERSION_MAJOR.$VERSION_MINOR"
 
 function package {
     ARCH="$1"
