b7ca884b007770802d1e82189f903ec212a5e8fc
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-06-07 11:19:21 +0300

    Wait for internet connection

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Runtime;
+using System.Threading;
 
 using NuciCLI;
 
@@ -15,8 +17,12 @@ namespace SteamGiveawaysBot.Launcher
         static string[] SecretKeyOptions = { "-k", "--ssk", "--key", "--secretkey", "--sharedsecretkey" };
         static string[] PlatformOptions = { "-p", "--platform" };
         
+        static TimeSpan ConnectionWaitTime => TimeSpan.FromSeconds(30);
+        
         static void Main(string[] args)
         {
+            WaitForInternet();
+
             ApplicationSettings settings = GetSettings(args);
             Launcher launcher = new Launcher(settings);
             BotUpdater updater = new BotUpdater(settings);
@@ -60,6 +66,30 @@ namespace SteamGiveawaysBot.Launcher
             }
 
             return split[1].Substring(0, split[1].Length - 2);
+        }
+
+        static void WaitForInternet()
+        {
+            bool isConnected = false;
+
+            while (!isConnected)
+            {
+                try
+                {
+                    using (var client = new WebClient())
+                    using (client.OpenRead("http://clients3.google.com/generate_204"))
+                    {
+                        isConnected = true;
+                        return;
+                    }
+                }
+                catch
+                {
+                    isConnected = false;
+                }
+
+                Thread.Sleep((int)ConnectionWaitTime.TotalMilliseconds);
+            }
         }
     }
 }
