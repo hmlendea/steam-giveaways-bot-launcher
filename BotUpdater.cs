c860ce202281e1639ca472d79631334e3b36a99b
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-09-01 17:50:14 +0300

    Changed the endpoint

--- BotUpdater.cs
+++ BotUpdater.cs
@@ -10,8 +10,8 @@ namespace SteamGiveawaysBot.Launcher
     public sealed class BotUpdater
     {
         const string LatestVersionFileNameFormat = "steam-giveaways-bot_{0}_{1}.zip";
-        const string LatestVersionArchiveUrlFormat = "http://hmlendea.go.ro/sgb/{0}";
-        const string LatestVersionStringUrl = "http://hmlendea.go.ro/sgb/version";
+        const string LatestVersionArchiveUrlFormat = "http://stayback.zapto.org/sgb/{0}";
+        const string LatestVersionStringUrl = "http://stayback.zapto.org/sgb/version";
 
         readonly ApplicationSettings settings;
 
