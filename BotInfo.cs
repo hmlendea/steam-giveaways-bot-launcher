a3fc284292a932ce9ddb143d24b4d8911945edec
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-05-01 15:14:45 +0300

    Removed unused code

--- BotInfo.cs
+++ BotInfo.cs
@@ -4,15 +4,12 @@ namespace SteamGiveawaysBot.Launcher
 {
     public static class BotInfo
     {
-        const string AppSettingsFileName = "appsettings.json";
         const string VersionFileName = "version.txt";
         const string RootDirectoryName = "sgb_app";
 
         public static string Name = nameof(SteamGiveawaysBot);
 
         public static string RootDirectory => Path.Combine(LauncherInfo.RootDirectory, RootDirectoryName);
-
-        public static string AppSettingsFilePath => Path.Combine(RootDirectory, AppSettingsFileName);
 
         public static string VersionFilePath => Path.Combine(LauncherInfo.RootDirectory, VersionFileName);
 
